45c0a24c8c136575f693b24de3ea7af7d3b988a2
Antoine <antbury28@gmail.com>
2022-05-10 18:03:35 +0200

    génération des classes à partir du banque.xsd

---
+++ gateway-soap/src/main/java/fr/insa/gatewaysoap/obj/RemboursementRequest.java
@@ -0,0 +1,64 @@
+
+package fr.insa.gatewaysoap.obj;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="operation" type="{http://spring.io/guides/gs-producing-web-service}operation"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "operation"
+})
+@XmlRootElement(name = "remboursementRequest", namespace = "http://spring.io/guides/gs-producing-web-service")
+public class RemboursementRequest {
+
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service", required = true)
+    protected Operation operation;
+
+    /**
+     * Obtient la valeur de la propriété operation.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Operation }
+     *     
+     */
+    public Operation getOperation() {
+        return operation;
+    }
+
+    /**
+     * Définit la valeur de la propriété operation.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Operation }
+     *     
+     */
+    public void setOperation(Operation value) {
+        this.operation = value;
+    }
+
+}
