02750d3a3ac186a43b28fa982b716830508da85b
Antoine <antbury28@gmail.com>
2022-05-10 18:16:50 +0200

    ajout endpoint

---
+++ gateway-soap/src/main/java/fr/insa/gatewaysoap/endpoint/GatewayEndpoint.java
@@ -0,0 +1,36 @@
+package fr.insa.gatewaysoap.endpoint;
+
+import fr.insa.gatewaysoap.obj.DebitRequest;
+import fr.insa.gatewaysoap.obj.DebitResponse;
+import fr.insa.gatewaysoap.obj.RemboursementRequest;
+import fr.insa.gatewaysoap.obj.RemboursementResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class GatewayEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public GatewayEndpoint() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "debitRequest")
+    @ResponsePayload
+    public DebitResponse debiterCompte(@RequestPayload DebitRequest request) {
+        return restTemplate.postForObject("http://localhost:8070/comptes/debit", request, DebitResponse.class);
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "remboursementRequest")
+    @ResponsePayload
+    public RemboursementResponse rembourserCompte(@RequestPayload RemboursementRequest request) {
+        return restTemplate.postForObject("http://localhost:8070/comptes/remboursement", request, RemboursementResponse.class);
+    }
+}
