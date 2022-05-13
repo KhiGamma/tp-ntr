4ea3da55a965a83091dadc5ef281a62874bf817d
ArnaudJuilien <61822174+ArnaudJuilien@users.noreply.github.com>
2022-05-13 09:56:17 +0200

    ajout exception + fichier data(vide)

---
+++ banque-service/src/main/java/fr/insa/banqueservice/exceptions/BadRequestException.java
@@ -0,0 +1,15 @@
+package fr.insa.banqueservice.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@Setter
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
